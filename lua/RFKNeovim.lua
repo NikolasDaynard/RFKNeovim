0cf3f4e0ed76fdc7ec42bd204921e397b5073ffe
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-10 13:59:26 -0500

    test

--- lua/RFKNeovim.lua
+++ lua/RFKNeovim.lua
@@ -5,11 +5,27 @@ local M = {}
 function M.setup(options)
   M.objectsPer100Lines = options.objectsPer100Lines or M.objectsPer100Lines or 20
 
+  -- excludes unicode characters from the completion
+  M.limit_Characterset = options.limit_Characterset
+  if M.limit_Characterset == nil then
+    M.limit_Characterset = true
+  end
+  -- allow the user to set the a custom Characterset
+  M.customCharacterset = options.limit_Characterset
+  if M.customCharacterset ~= nil then
+    M.limit_Characterset = true
+  else
+    M.customCharacterset =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_!`~@#$%^&*()-=+[{]}\\|;:\'\",<.>/?'
+  end
 end
 
 -- Define a Lua function to handle cursor movement
 function M.handle_cursor_moved()
-  print(vim.api.nvim_buf_get_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), true)[1]:sub(vim.fn.col('.'), vim.fn.col('.')))
+  local char = vim.api.nvim_buf_get_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), true)[1]:sub(vim.fn.col('.'), vim.fn.col('.'))
+  if string.find(M.customCharacterset, char) == nil then
+    print('Cursor moved to a non-alphanumeric character')
+  end
 end
 
 -- Attach the Lua function to the CursorMoved event
