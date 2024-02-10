2b4db981b40093744bb80118230a7116ef108fcf
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-10 16:45:59 -0500

    update items

--- lua/RFKthings.lua
+++ lua/RFKthings.lua
@@ -14,6 +14,14 @@ M.RFKthings = {
   "It's an animal! Sadly though, not the one you are looking for",
   "It's a very riced unix system",
   "It's nothing, just some text",
+  "It's something that you can't see",
+  "It's a way to exit vim",
+  "It's Neovim running on a Nintendo 3DS",
+  "It's a colorscheme with a contrast rating of 2.3",
+  "It's a terminal with 256 colors",
+
+  
+  "There seems to be nothing but noise here",
   -- Add more data as needed
 }
 
