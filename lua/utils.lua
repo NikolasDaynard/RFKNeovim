0b6e8083526eac1e511f44205c756f96fdafabb5
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-10 14:15:45 -0500

    test

--- lua/utils.lua
+++ lua/utils.lua
@@ -4,12 +4,17 @@ local M = {}
 
 -- checks if other buffers are open
 function M.loadLine(line)
+
+end
+
+
+function M.closeAllOpenTerminals() -- WIP
   local file = io.open("things.txt", "r")
 
   if file then
     -- Read the specified line
     local readLine
-    for i = 1, line do
+    for i = 1, 3 do
       readLine = file:read()
       if not readLine then
         break  -- Stop if end of file is reached before desired line
@@ -27,15 +32,6 @@ function M.loadLine(line)
     end
   else
     print("Failed to open file.")
-  end
-end
-
-
-function M.closeAllOpenTerminals() -- WIP
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if string.find(string.lower(vim.api.nvim_buf_get_name(buf)), 'gradlew') and M.hasOtherOpenBuffers() and vim.api.nvim_buf_get_name(buf) then
-      vim.cmd(':q')
-    end
   end
 end
 
