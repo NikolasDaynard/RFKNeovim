30dd2bd8823c43994beebe028989e35fb6066ad7
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-10 16:20:18 -0500

    test

--- lua/RFKutils.lua
+++ lua/RFKutils.lua
@@ -4,14 +4,13 @@ local M = {}
 
 local things = require'things'
 
--- rand() % 16 = 0-15
 function M.generateRandomLineForCharacters(characterSet)
   local usedLines = {}
   for i = 1, #characterSet do
     local char = characterSet:sub(i,i)
     -- local randomLine(vim.cmd(rand() % things.getThingsLength)) + 1
     -- if !usedLines.contains(randomLine) then
-      usedLines.append((rand() % things.getThingsLength) + 1) -- indexed from 1
+      usedLines.append(math.random(1, things.getThingsLength())) -- indexed from 1
     -- end
   end
   return usedLines
