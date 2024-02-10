0828472bde3df7a44e2dd4f69b2c2be285d730db
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-10 15:33:55 -0500

    test

--- lua/RFKNeovim.lua
+++ lua/RFKNeovim.lua
@@ -26,8 +26,8 @@ end
 -- Define a Lua function to handle cursor movement
 function M.handle_cursor_moved()
   local char = vim.api.nvim_buf_get_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), true)[1]:sub(vim.fn.col('.'), vim.fn.col('.'))
-  if string.find(M.customCharacterset, char) ~= nil then
-    -- print(things.getData(2))
+  if string.find(M.customCharacterset, char, 1, true) then
+      print(things.getData(2))
   end
 end
 
