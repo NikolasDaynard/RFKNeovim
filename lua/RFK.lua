d95f9bd85446f0fdf244ac009922198a30fbe826
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-10 12:55:25 -0500

    cursor callback

--- lua/RFK.lua
+++ lua/RFK.lua
@@ -6,8 +6,20 @@ local utils = require'utils'
 local vendorDep = require'vendorDep'
 
 function M.setup(options)
-  M.terminal_size = options.terminal_size or M.terminal_size or 60
+  M.objectsPer100Lines = options.objectsPer100Lines or M.objectsPer100Lines or 20
 
 end
+
+function M.on_mouse_move()
+  -- Your code here
+  print("Mouse moved!")
+end
+vim.cmd([[
+function! HandleCursorMoved()
+  " Your custom actions here
+  " For example, echoing the cursor position
+  echo "Cursor moved to line " . line('.') . " column " . col('.')
+endfunction
+]])
 
 return M
