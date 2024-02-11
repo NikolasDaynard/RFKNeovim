357efecc68d74861c5e83b74e2c153f0b7d1af1b
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-11 10:35:43 -0500

    kitten hotfix

--- lua/RFKutils.lua
+++ lua/RFKutils.lua
@@ -7,11 +7,11 @@ local things = require'RFKthings'
 function M.generateRandomLineForCharacters(characterSet)
   math.randomseed(os.time())
   local usedLines = {}
-  for i = 2, #characterSet do -- 1 is the kitten
+  for i = 1, #characterSet do 
     local char = characterSet:sub(i,i)
     -- local randomLine(vim.cmd(rand() % things.getThingsLength)) + 1
     -- if !usedLines.contains(randomLine) then
-      usedLines[i] = (math.random(1, things.getThingsLength())) -- indexed from 1
+      usedLines[i] = (math.random(2, things.getThingsLength())) -- indexed from 2 because 1 is the kitten
     -- end
   end
   usedLines[math.random(1, #characterSet)] = 1 -- set one to be the kitten
